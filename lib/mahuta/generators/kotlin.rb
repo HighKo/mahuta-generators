a897e2458905e5b0d0dc51c27ce30ea7546f59d8
Max Trense <dev@trense.info>
2017-11-16 16:04:20 +0100

    Cleans up kotlin helpers

--- lib/mahuta/generators/kotlin.rb
+++ lib/mahuta/generators/kotlin.rb
@@ -17,9 +17,8 @@ module Mahuta::Generators
 
   module Kotlin
 
-    def kotlin_type(node)
-      type = node[:type]
-      case node[:many]
+    def kotlin_type(type, many = nil)
+      case many
       when nil, false
         kotlin_type_name(type)
       when true, :unordered
@@ -47,52 +46,12 @@ module Mahuta::Generators
         'DateTime'
       when :binary
         'ByteArray'
-      when :guid
+      when :guid, :uuid
         'UUID'
       when :structured_data
-        'JsonElement'
+        'Hash'
       else
         kotlin_class_name(type)
-      end
-    end
-
-    def is_builtin?(type)
-      case type
-      when :bool, :boolean, :int, :integer, :float, :long, :long_integer, :string, :email, :phone_number, :url, :date, :binary, :guid 
-        true
-      else
-        false
-      end
-    end
-
-    def is_primitive?(type)
-      case type
-      when :bool, :boolean, :int, :integer, :float, :long, :long_integer, :string, :email, :phone_number, :structured_data
-        true
-      else
-        false
-      end
-    end
-
-    def is_extern?(type)
-      case type
-      when :date, :guid, :structured_data, :url
-        true
-      else 
-        false
-      end
-    end
-
-    def fully_qualified_extern(type)
-      case type
-      when :date
-        'org.joda.time.DateTime'
-      when :guid
-        'java.util.UUID'
-      when :structured_data
-        'com.google.gson.JsonElement'
-      when :url
-        'java.net.URL'
       end
     end
 
@@ -100,10 +59,22 @@ module Mahuta::Generators
         type.to_s.camelize(:upper)
     end
 
-    def kotlin_namespace(node, *postfixes)
-      [ *node.namespace, *postfixes ].collect {|nc| nc.to_s.camelize(:lower) }.join('.')
+    def kotlin_file_name(name) 
+      "#{kotlin_type_name(name)}.kt"
+    end
+    
+    def kotlin_directory(namespace)
+      File.join(namespace.collect {|nc| nc.to_s.camelize(:lower) })
     end
 
+    def kotlin_namespace(namespace)
+      namespace.collect {|nc| nc.to_s.camelize(:lower) }.join('.')
+    end
+    
+    def kotlin_full_qualified_type(namespace, type)
+      [kotlin_namespace(namespace), kotlin_class_name(type)].join('.')
+    end
+    
     def kotlin_variable_name(name)
       name.to_s.camelize(:lower)
     end
@@ -111,130 +82,6 @@ module Mahuta::Generators
     def kotlin_constant_name(name)
       name.to_s.upcase
     end
-
-    def kotlin_serializer_function(node) 
-      case node.type
-      when :guid, :date, :url
-        'toString'
-      when :structured_data
-        nil
-      else 
-        'toJson'
-      end
-    end
-
-    def kotlin_json_adder_function(node)
-      if is_builtin? node.type 
-        'addProperty' 
-      else 
-        'add'
-      end
-    end
-
-    def kotlin_deserializer_function(node) 
-      case node.type
-      when :binary
-        'byteArrayFromJson'
-      when :guid
-        'UUID.fromString'
-      when :date
-        'DateTime.parse'
-      when :url
-        'URL'
-      else 
-        'fromJson' 
-      end
-    end
-
-    def kotlin_json_fetcher_function(node)
-      type = node.type
-      
-      # Fixes enum issue. 
-      # TODO clean up type search
-      unless is_builtin?(type)
-        begin
-          target_type = find_type_node_for(node, node.root)
-          if target_type.node_type == :enumeration
-            return 'asJsonPrimitive'
-          end
-        rescue
-        end
-        return 'asJsonObject' 
-      end
-
-      case node.type
-      when :bool, :boolean
-        'asBoolean'
-      when :int, :integer
-        'asInt'
-      when :float
-        'asFloat'
-      when :long, :long_integer
-        'asLong'
-      when :string, :email, :phone_number, :url, :date, :binary, :guid
-        'asString'
-      end
-    end
-
-    def scope(node, type) 
-      node.ascendants {|p| p.node_type == type}.first
-    end
-
-    def format_kotlin_import(node)
-      kotlin_namespace(node) + '.' + kotlin_class_name(node.name)
-    end
-
-    def kotlin_import(node)
-      return fully_qualified_extern(node.type) if is_extern? node.type 
-      return nil if is_builtin? node.type 
-
-      type_node = find_type_node_for(node, node.root)
-
-      format_kotlin_import(type_node)
-    end
-
-    def kotlin_property_import(node, scope = nil)
-      return fully_qualified_extern(node.type) if is_extern? node.type 
-      return nil if is_builtin? node.type 
-
-      # by default, lookup the import in the node's aggregate
-      scope ||= scope(node, :aggregate)
-      type_node = find_type_node_for(node, scope)
-
-      format_kotlin_import(type_node)
-    end
-
-    def kotlin_file_name(node) 
-      "#{kotlin_type_name(node[:name])}.kt"
-    end
-
-    def find_type_node_for(node, scope = nil)
-      if scope.nil?
-        # if not given scope, look it up in all namespaces
-        type_node = node
-          .root
-          .descendants(:namespace).flat_map {|ns|
-            find_type_node_for(node, ns)
-          }
-          .first
-      else
-        type_node = scope
-          .descendants {|descendant|
-            descendant.name == node.type && descendant&.is_value_type? rescue false
-          }
-          .first
-      end
-
-      unless type_node.respond_to? :namespace 
-        raise <<-EOF.strip_heredoc
-          Can't infer namespace for #{node.inspect} in scope #{scope.inspect}
-          while generating #{node.parent.inspect}.
-        EOF
-      end
-
-      type_node
-    end
-
 
   end
 
